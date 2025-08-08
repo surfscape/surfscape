6d72131bca880cc1e4f42d06bb6cbadeee311d4e
ReduxFlakes <tiagompca@proton.me>
2025-08-08 23:58:05 +0100

    doing some testing (cloudflare workers)

---
+++ worker.js
@@ -0,0 +1,14 @@
+import { getAssetFromKV } from '@cloudflare/kv-asset-handler';
+
+export default {
+    async fetch(request, env, ctx) {
+        try {
+            return await getAssetFromKV(
+                { request, waitUntil: ctx.waitUntil.bind(ctx) },
+                { ASSET_NAMESPACE: env.__STATIC_CONTENT, ASSET_MANIFEST: env.__STATIC_CONTENT_MANIFEST }
+            );
+        } catch (e) {
+            return new Response('Not Found', { status: 404 });
+        }
+    }
+};
